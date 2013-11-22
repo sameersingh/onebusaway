7c0938158e85e727f3f39dfb343f062b7a956b3a
nacho cano <nachoacano@gmail.com>
2013-11-21 23:42:45 -0800

    adding ova values. If using no predictors, the error match. If start adding predictors, our is better than oba

--- matlab/autoregressive/main.m
+++ matlab/autoregressive/main.m
@@ -13,20 +13,26 @@ function main(filename, model_order)
     
     % Train error
     Y_hat_train = X_train * w;
-    train_rmse = sqrt(mean((Y_hat_train - Y_train) .^ 2));
-    train_adj_rSquared = adj_rSquared(Y_train, Y_hat_train, model_order); 
+    train_rmse_our = sqrt(mean((Y_hat_train - Y_train) .^ 2));
+    train_adj_rSquared_our = adj_rSquared(Y_train, Y_hat_train, model_order); 
+    train_rmse_oba = sqrt(mean(Y_train .^2));
     
     % Test error
     Y_hat_test = X_test * w;
-    test_rmse = sqrt(mean((Y_hat_test - Y_test) .^ 2));
-    test_adj_rSquared = adj_rSquared(Y_test, Y_hat_test, model_order); 
+    test_rmse_our = sqrt(mean((Y_hat_test - Y_test) .^ 2));
+    test_adj_rSquared_our = adj_rSquared(Y_test, Y_hat_test, model_order);
+    test_rmse_our_oba = sqrt(mean(Y_test .^2));
+
     
     % Print results
     fprintf('model order: %d\n', model_order);
     fprintf('coefficients:\n'); disp(w)
-    fprintf('training RMSE: %d\n', train_rmse);
-    fprintf('training Adj R-Squared: %d\n', train_adj_rSquared);
-    fprintf('test RMSE: %d\n', test_rmse);
-    fprintf('test Adj R-Squared: %d\n', test_adj_rSquared);
+    fprintf('training RMSE OUR: %d\n', train_rmse_our);
+    fprintf('training Adj R-Squared OUR: %d\n', train_adj_rSquared_our);
+    fprintf('test RMSE OUR: %d\n', test_rmse_our);
+    fprintf('test Adj R-Squared OUR: %d\n', test_adj_rSquared_our);
+    fprintf('training RMSE OBA: %d\n', train_rmse_oba);
+    fprintf('test RMSE OBA: %d\n', test_rmse_our_oba);
+
     
 end
