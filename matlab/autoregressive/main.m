02d985776ef6c9fa67fefd856226b9c684ae8988
nacho cano <nachoacano@gmail.com>
2013-11-22 00:26:48 -0800

    changing OBA y_hat… they predict the current delay reported.. shifting y’s 1 place…

--- matlab/autoregressive/main.m
+++ matlab/autoregressive/main.m
@@ -15,14 +15,18 @@ function main(filename, model_order)
     Y_hat_train = X_train * w;
     train_rmse_our = sqrt(mean((Y_hat_train - Y_train) .^ 2));
     train_adj_rSquared_our = adj_rSquared(Y_train, Y_hat_train, model_order); 
-    train_rmse_oba = sqrt(mean(Y_train .^2));
+    %train_rmse_oba = sqrt(mean(([Y_train(1);diff(Y_train)] - Y_train) .^2));
+    %train_rmse_oba = sqrt(mean(Y_train .^2));
+    train_rmse_oba = sqrt(mean(([0;Y_train(1:end-1)] - Y_train) .^2));
+
     
     % Test error
     Y_hat_test = X_test * w;
     test_rmse_our = sqrt(mean((Y_hat_test - Y_test) .^ 2));
     test_adj_rSquared_our = adj_rSquared(Y_test, Y_hat_test, model_order);
-    test_rmse_our_oba = sqrt(mean(Y_test .^2));
-
+    %test_rmse_oba = sqrt(mean([Y_test(1);diff(Y_test)] .^2));  
+    %test_rmse_oba = sqrt(mean(Y_test .^2));
+    test_rmse_oba = sqrt(mean(([0;Y_test(1:end-1)] - Y_test) .^2));
     
     % Print results
     fprintf('model order: %d\n', model_order);
@@ -32,7 +36,7 @@ function main(filename, model_order)
     fprintf('test RMSE OUR: %d\n', test_rmse_our);
     fprintf('test Adj R-Squared OUR: %d\n', test_adj_rSquared_our);
     fprintf('training RMSE OBA: %d\n', train_rmse_oba);
-    fprintf('test RMSE OBA: %d\n', test_rmse_our_oba);
+    fprintf('test RMSE OBA: %d\n', test_rmse_oba);
 
     
 end
